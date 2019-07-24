1ab901c36dba2738f58b599d302f611e293e2aa0
QUEZADA GONZALEZ <L.QUEZADA@CIMMYT.ORG>
2019-07-24 12:59:11 -0500

    default client construction

--- src/app/containers/defaultContainer/defaultContainer.js
+++ src/app/containers/defaultContainer/defaultContainer.js
@@ -1,15 +1,45 @@
 import React from 'react';
 
+//import clients
+import _myClient from "../../../clients/clientDefault/clientDefault";
+
+//import components
+import LoadingComponent from "../../components/LoadingComponent";
+
+//create client objects
+const myClient = new _myClient();
+
 class DefaultContainer extends React.Component{
   constructor(){
     super();
     this.state = {
-      containerName : "defaultContainer"
+      containerName : "defaultContainer",
+      data : undefined
     }
   }
+
+  componentDidMount(){
+    myClient.getData(res => {
+      this.setState({
+        data : res
+      });
+    });
+  }
+
   render(){
+
+    let myData = this.state.data === undefined ?
+    (<LoadingComponent/>)
+    :
+    (<h1>Data!</h1>)
+
     return(
-      <h1>Hello!, I am {this.state.containerName}</h1>
+      <React.Fragment>
+        <h1>Hey!, I am {this.state.containerName}</h1>
+        <div>
+          {myData}
+        </div>
+      </React.Fragment>
     );
   }
 }
---
+++ src/clients/clientDefault/modelDefault.js
@@ -0,0 +1,11 @@
+class modelDefault{
+
+  constructor(comments, id, firstName, lastName){
+    this.comments = comments,
+    this.id = id,
+    this.firstName = firstName,
+    this.lastName = lastName
+  }
+}
+
+export default modelDefault;
